725bd906446db84c512a266100a6671eb31a1156
Ismael Burgos <ismaelburgosroldan@gmail.com>
2019-01-23 12:23:15 +0100

    Create LCD2.cpp

---
+++ LCD2.cpp
@@ -0,0 +1,34 @@
+#include "mbed.h"
+#include "TextLCD.h"
+
+DigitalOut myled(LED1);
+
+#ifdef TARGET_KL25Z
+    TextLCD lcd(PTA13, PTD5, PTA4, PTA5, PTC8, PTC9);
+    AnalogIn button(PTB0);
+    PwmOut backlight(PTD0);
+#endif
+
+int main()
+{
+#ifdef TARGET_KL25Z    
+    lcd.cls();
+    wait(0.5);
+#endif
+    while (1)
+    {
+
+#ifdef TARGET_KL25Z
+        unsigned long valueint = button.read_u16();
+        float valuereal = button.read();
+        lcd.cls();
+        lcd.locate(0,0);
+        lcd.printf("BUTTON : %d", valueint);
+       
+        lcd.locate(0,1);
+        lcd.printf("BUTTON : %f", valuereal);
+        backlight = valuereal;
+#endif
+        wait(0.2);
+    }
+}
